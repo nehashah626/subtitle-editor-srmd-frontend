9833ea806f40a9daaec06f5719038d531a698cb4
nehashah626 <57422463+nehashah626@users.noreply.github.com>
2023-11-10 10:06:22 -0600

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,9 @@
+import sum from './sum';
+
+it('sums numbers', () => {
+  expect(sum(1, 2)).toEqual(3);
+  expect(sum(2, 2)).toEqual(4);
+});
 /*
 import { render, screen } from '@testing-library/react';
 import App from './App';
