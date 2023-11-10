05872948c9067b2d8c4382915c2ef0a5c0ec8672
nehashah626 <57422463+nehashah626@users.noreply.github.com>
2023-11-10 10:09:53 -0600

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
-import sum from './sum';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 
-it('sums numbers', () => {
-  expect(sum(1, 2)).toEqual(3);
-  expect(sum(2, 2)).toEqual(4);
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
 });
 /*
 import { render, screen } from '@testing-library/react';
