86152220a11cc6836256aff60910267de18e0500
nehashah626 <57422463+nehashah626@users.noreply.github.com>
2023-11-11 15:04:07 -0600

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,19 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "./App";
+
+test("renders App component with SubtitleCreator", () => {
+  // Render the App component
+  const { getByText } = render(<App />);
+
+  // Check if the SubtitleCreator is present in the rendered component
+  const subtitleCreatorElement = getByText(/SubtitleCreator/i);
+  expect(subtitleCreatorElement).toBeInTheDocument();
+});
+
+// You can add more tests based on your component's functionality
+
+/*
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
@@ -5,14 +21,5 @@ import App from './App';
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
-});
-/*
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
 });
 */
